a57d31d3a3c3664b2ccda9b78379e0743177cf9f
Antonio Bonifacio Hernández <abhernandez@stefanini.com>
2023-05-10 12:21:53 -0600

    First commit

---
+++ src/main/java/com/honda/olympus/ms/logevent/Application.java
@@ -0,0 +1,13 @@
+package com.honda.olympus.ms.logevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
