2849c3e05830ef2e68125ccaec09c5dae599d72a
anttoniobh <abhernandez@stefanini.com>
2023-05-27 13:11:36 -0600

    Update controller classes

--- src/main/java/com/honda/olympus/ms/logevent/controller/LogController.java
+++ src/main/java/com/honda/olympus/ms/logevent/controller/LogController.java
@@ -1,5 +1,7 @@
 package com.honda.olympus.ms.logevent.controller;
 
+import static java.util.stream.Collectors.joining;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.honda.olympus.ms.logevent.domain.Event;
 import com.honda.olympus.ms.logevent.service.LogService;
-import com.honda.olympus.ms.logevent.util.HttpUtil;
 
 
 @RestController
@@ -27,10 +30,19 @@ public class LogController
 	@PostMapping(path = "/event", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> logEvent(@Valid @RequestBody Event event, Errors errors) 
 	{
-		HttpUtil.handleBadRequest(errors);
+		handleBadRequest(errors);
 		this.logService.logEvent(event);
 		
 		return ResponseEntity.ok(HttpStatus.OK.getReasonPhrase());
+	}
+	
+	
+	
+	public void handleBadRequest(Errors errors) {
+		if (errors.hasErrors()) {
+			String messages = errors.getFieldErrors().stream().map(FieldError::getDefaultMessage).collect(joining("; "));
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, messages);
+		}
 	}
 	
 }
